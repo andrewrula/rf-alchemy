32dd6303f09f71f199adf81c3c8061698f9559e5
Andrew Rula <andrewrula@gmail.com>
2024-05-26 18:04:25 -0400

    Cleaned up incrementResource

--- rf-alchemy-main.js
+++ rf-alchemy-main.js
@@ -24,35 +24,38 @@ var resourceBank = {
 
 //Generic Resource Go Up By (number)
 function incrementResource(resource, increment){
+    //Declare some vars
     workingResource = resource
     workingResourceCostType = workingResource.costType
     workingResourceCostQuant = workingResource.cost
+//Check if there is a cost. If no cost, continue on. Else, check if we have enough. If we don't, evac function. 
+        //TODO: Multi-cost-support.
+        //TODO: Catalyst support
     if(workingResourceCostType != "none"){
         if(resourceBank[workingResourceCostType].quant < workingResourceCostQuant){
         console.log ("Not Enough " + workingResourceCostType)
         return;
-        }
-    }
+        }        
+//If it's not 'none' and we have enough, reduce those costs by the cost amount, and push to the html
+        else {
+        resourceBank[workingResourceCostType].quant = resourceBank[workingResourceCostType].quant - workingResourceCostQuant;
+        const costElement = document.getElementById(resourceBank[workingResourceCostType].id);
+        costElement.textContent = resourceBank[workingResourceCostType].quant;
+    };
+};
+//Now we check if we're going to hit capacity. If yes, turn working number into the capacity. If no, change working number into working number + increment. 
     if(workingResource.quant + increment >= workingResource.capacity){
         workingResource.quant = resource.capacity;
-        if(workingResourceCostType != "none"){
-        resourceBank[workingResourceCostType].quant = resourceBank[workingResourceCostType].quant - workingResourceCostQuant;
-        };
-        console.log("Capped!")
+        console.log("Resource incremented but is capped!")
         }
         else{
-        if(workingResourceCostType != "none"){
-            resourceBank[workingResourceCostType].quant = resourceBank[workingResourceCostType].quant - workingResourceCostQuant;
-            };
         workingResource.quant = workingResource.quant + increment
+        console.log("Resource incremented.")
         };
+//Adjust resource as needed and push to html
     resource = workingResource;
     const resourceElement = document.getElementById(resource.id);
     resourceElement.textContent = resource.quant;
-    if(workingResourceCostType != "none"){
-        const costElement = document.getElementById(resourceBank[workingResourceCostType].id);
-        costElement.textContent = resourceBank[workingResourceCostType].quant;
-        };
 };
 
 function unitTest (){
