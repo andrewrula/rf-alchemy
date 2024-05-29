334ed113c525f51f656a8298fa20994321de9f8a
Andrew Rula <andrewrula@gmail.com>
2024-05-29 15:00:17 -0400

    Initial Visiblilty Implementation

--- rf-alchemy-main.js
+++ rf-alchemy-main.js
@@ -1,138 +1,191 @@
 //Resource Manager
 var resourceBank = {
-    wood:{
-        quant: 0,
-        capacity: 20,
-        id: "woodCount"
-    },
     energy:{
         quant: 0,
         capacity: 20,
-        id: "energyCount"
+        id: "energyCount",
+        visible: true,  
+    },
+    wood:{
+        quant: 0,
+        capacity: 20,
+        id: "woodCount",
+        visible: false,
+        visibilityTrigger: "energy",
+        visibilityTarget: "woodVis",
     },
     gold:{
         quant: 0,
         capacity: 1000,
-        id: "goldCount"
+        id: "goldCount",
+        visible: false,
+        visibilityTrigger: "wood",
+        visibilityTarget: "goldVis",
     },
     exploration:{
         quant: 0,
         capacity: 1000,
-        id: "exploreCount"
+        id: "exploreCount",
+        visible: false,
     },
     stamina:{
         quant: 0,
         capacity: 50,
-        id: "staminaCount"
+        id: "staminaCount",
+        visible: false,
     },
     strength:{
         quant: 0,
         capacity: 50,
-        id: "strengthCount"
+        id: "strengthCount",
+        visible: false,
     },
     dirt:{
         quant: 0,
         capacity: 5000,
-        id: "dirtCount"
+        id: "dirtCount",
+        visible: false,
     },
     crystal:{
         quant: 0,
         capacity: 5,
-        id: "crystalCount"
+        id: "crystalCount",
+        visible: false,
     },
     blacksmithReputation:{
         quant: 0,
         capacity: 100,
-        id: "blacksmithReputation"
+        id: "blacksmithReputation",
+        visible: false,
     },
     innkeepReputation:{
         quant: 0,
         capacity: 100,
-        id: "inkeepReputation"
+        id: "inkeepReputation",
+        visible: false,
     },
     herbalistReputation:{
         quant: 0,
         capacity: 100,
-        id: "herbalistReputation"
+        id: "herbalistReputation",
+        visible: false,
     },
     hunterReputation:{
         quant: 0,
         capacity: 100,
-        id: "hunterReputation"
+        id: "hunterReputation",
+        visible: false,
     },
     warlordReputation:{
         quant: 0,
         capacity: 100,
-        id: "warlordReputation"
+        id: "warlordReputation",
+        visible: false,
     },
 }
 
 //Utility Functions
     //Generic Resource Go Up By (number)
-        function incrementResource(resource, increment){
-        //Setup Vars
-            var workingResource = resourceBank[resource];
-            if(workingResource.quant + increment >= workingResource.capacity){
-                workingResource.quant = resourceBank[resource].capacity;
-                console.log("Resource incremented but hit cap!");
-            }
-            else{
-                workingResource.quant = workingResource.quant + increment
-                console.log("Resource incremented.");
-            };
-            updateResourceBank(resource, workingResource);
-            updateResourceHTML(resource);
+function incrementResource(resource, increment){
+//Setup Vars
+    var workingResource = resourceBank[resource];
+    if(workingResource.quant + increment >= workingResource.capacity){
+        workingResource.quant = resourceBank[resource].capacity;
+        console.log("Resource incremented but hit cap!");
+    }
+    else{
+        workingResource.quant = workingResource.quant + increment
+        console.log("Resource incremented.");
     };
+    updateResourceBank(resource, workingResource);
+    updateResourceHTML(resource);
+    updateVisibility(resource);
+};
 
-    //Checks and Pays Cost 
-        function payCost(resource, neededValue){
-        //Checks if cost can be paid
-        if(checkCost(resource,neededValue) == false){
-            console.log("Failed to Pay Cost")
-            return false;
-        };
-        //Checks if cost is none
+//Checks and Pays Cost 
+    function payCost(resource, neededValue){
+    //Checks if cost can be paid
+    if(checkCost(resource,neededValue) == false){
+        console.log("Failed to Pay Cost")
+        return false;
+    };
+    //Checks if cost is none
+    if(resource == "none"){
+        return true;
+    };
+    //Update resourceBank
+        //TODO: Update to use updateResourceBank()
+    resourceBank[resource].quant = resourceBank[resource].quant - neededValue;
+    updateResourceHTML(resource);
+    console.log ("Cost Paid")
+};
+
+//Checks if we have sufficient resource. Returns boolean.
+    function checkCost(resource, neededValue){
         if(resource == "none"){
+            return true;}
+        else if(resourceBank[resource].quant >= neededValue){
             return true;
-        };
-        //Update resourceBank
-            //TODO: Update to use updateResourceBank()
-        resourceBank[resource].quant = resourceBank[resource].quant - neededValue;
-        updateResourceHTML(resource);
-        console.log ("Cost Paid")
-    };
-
-    //Checks if we have sufficient resource. Returns boolean.
-        function checkCost(resource, neededValue){
-            if(resource == "none"){
-                return true;}
-            else if(resourceBank[resource].quant >= neededValue){
-                return true;
-                }
-            else if (resourceBank[resource].quant < neededValue){
-                console.log("Not enough " + resource)
-                return false
             }
-    };
+        else if (resourceBank[resource].quant < neededValue){
+            console.log("Not enough " + resource)
+            return false
+        }
+};
 
-    //Updates resourceBank
-        function updateResourceBank(resourceID, newResourceObject){
-            resourceBank[resourceID] = newResourceObject;
-    };
+//Updates resourceBank
+    function updateResourceBank(resourceID, newResourceObject){
+        resourceBank[resourceID] = newResourceObject;
+};
 
-    //Updates HTML for provided resource from resourceBank
-        function updateResourceHTML(resource){
-        const costElement = document.getElementById(resourceBank[resource].id);
-        costElement.textContent = resourceBank[resource].quant;
-    };
+//Updates HTML for provided resource from resourceBank
+function updateResourceHTML(resource){
+    const costElement = document.getElementById(resourceBank[resource].id);
+    costElement.textContent = resourceBank[resource].quant;
+};
 
-        function checkCatalyst(resource, neededValue){//NYI
 
-    };
 
-        function setResource(resource, setValue, ignoreCap){//NYI
-        
+function checkCatalyst(resource, neededValue){//NYI
+
+};
+
+function setResource(resource, setValue, ignoreCap){//NYI
+    
+};
+
+function setVisible(resource){
+    console.log("Checking if " + resource + " is visible");    
+    if(resourceBank[resource].visible == false){
+        console.log(resource + " is not visible. Setting to true!")
+        resourceBank[resource].visible = true
     };
+};
+
+function setHidden(resource){
+    if(resourceBank[resource].visible == true){
+        resourceBank[resource].visible = false
+    };
+};
+
+function updateVisibility(visTrigger){
+    for(const resource in resourceBank){
+        if(resourceBank[resource].visibilityTrigger == visTrigger){
+            setVisible(resource);
+            updateVisibilityHTML(resource)
+        };
+    }
+
+function updateVisibilityHTML(resource){
+    const visElement = document.getElementById(resourceBank[resource].visibilityTarget);
+    visElement.style.setProperty('display','block')
+};
+
+    //if <visibilityTrigger> == resource
+        //setVisible(that resource)
+        //updateVisibilityinHTML
+    
+};
 
 
 
